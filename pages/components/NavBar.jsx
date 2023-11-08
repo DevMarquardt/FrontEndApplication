533829c566a1de5c23ef5922aad4a0c61659146a
Gabriel Fellipe Marquardt <gfellipe@weg.net>
2023-11-07 21:48:55 -0300

    first commit

---
+++ pages/components/NavBar.jsx
@@ -0,0 +1,19 @@
+export default function NavBarComponent(){
+    return(
+        <>
+        <nav className="h-[3.5rem]">
+            <ul className="flex justify-center items-center h-[100%] space-x-10">
+                <li>
+                    <a href="">page 1</a>
+                </li>
+                <li>
+                    <a href="">page 2</a>
+                </li>
+                <li>
+                    <a href="">page 3</a>
+                </li>
+            </ul>
+        </nav>
+        </>
+    )
+}
